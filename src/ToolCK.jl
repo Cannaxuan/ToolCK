571870d208c0c6fa8def74eb59f1e9cbcf9dd3ae
CaesarKang <nusleo@163.com>
2019-05-21 17:57:07 +0800

    revise the module file

--- src/ToolCK.jl
+++ src/ToolCK.jl
@@ -1,19 +1,18 @@
 __precompile__()
 
-
 module ToolCK
 
 using PyCall, MAT
 
-include("corr_CK.jl"))
-include("readMat_CK.jl"))
-include("writeMat_CK.jl"))
-include("ismember_CK.jl"))
-include("nanstd_CK.jl"))
-include("nanMedian_CK.jl"))
-include("nanSum_CK.jl"))
-include("nanMean_CK.jl"))
-include("unique_CK.jl"))
+include("corr_CK.jl")
+include("readMat_CK.jl")
+include("writeMat_CK.jl")
+include("ismember_CK.jl")
+include("nanstd_CK.jl")
+include("nanMedian_CK.jl")
+include("nanSum_CK.jl")
+include("nanMean_CK.jl")
+include("unique_CK.jl")
 
 export corr_CK, readMat_CK, writeMat_CK, ismember_CK, nanstd_CK, nanSum_CK, nanMean_CK, nanMedian_CK, unique_CK
 
