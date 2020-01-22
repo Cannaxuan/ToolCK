5f7f60b5699b5ec23b58cf3b8876aa560939da92
Cannaxuan <43407710+Cannaxuan@users.noreply.github.com>
2020-01-22 16:57:18 +0800

    add five common functions

---
+++ src/split_data.jl
@@ -0,0 +1,53 @@
+function split_data(Arr,n)
+    ## Arr: the data you want to separate.
+    ## n: the number of elements/rows in each segment.
+
+    num = size(Arr, 1)
+    if num <= n
+        res = Vector{Array{eltype(Arr)}}(undef, 1)
+        res[1] = Arr
+    else
+
+        x, y = fldmod(num, n)
+        if y != 0
+            res = Vector{Array{eltype(Arr)}}(undef, x+1)
+
+            if ndims(Arr) == 1
+                for i in 1:x
+                    res[i] = Arr[(i-1)*n+1:i*n]
+                end
+                res[end] = Arr[x*n+1:x*n+y]
+
+            elseif ndims(Arr) == 2
+                for i in 1:x
+                    res[i] = Arr[((i-1)*n+1:i*n), :]
+                end
+                res[end] = Arr[(x*n+1:x*n+y), :]
+
+            elseif ndims(Arr) == 3
+                for i in 1:x
+                    res[i] = Arr[((i-1)*n+1:i*n), :, :]
+                end
+                res[end] = Arr[(x*n+1:x*n+y), :, :]
+            end
+        else
+            res = Vector{Array{eltype(Arr)}}(undef, x)
+            if ndims(Arr) == 1
+                for i in 1:x
+                    res[i] = Arr[(i-1)*n+1:i*n]
+                end
+            elseif ndims(Arr) == 2
+                for i in 1:x
+                    res[i] = Arr[((i-1)*n+1:i*n), :]
+                end
+            elseif ndims(Arr) == 3
+                for i in 1:x
+                    res[i] = Arr[((i-1)*n+1:i*n), :, :]
+                end
+            end
+        end
+
+    end
+
+    return res
+end
